6270aa26104d10733c553cf73e7e6c0c4d360e83
Olga Toporova <sova795@yandex.ru>
2022-08-22 02:50:03 +0500

    Выполнено задание 36

---
+++ Ex_36/Program.cs
@@ -0,0 +1,49 @@
+﻿/* Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+
+[3, 7, 23, 12] -> 19
+[-4, -6, 89, 6] -> 0 */
+
+class Program
+{
+    static void WriteArray(int[] array)
+    {
+        int sum = 0;
+        for (int i = 0; i < array.Length; i++)
+        {
+            Console.Write(array[i] + " ");
+            if (i % 2 != 0)
+            {
+                sum += array[i];
+            }
+        }
+        Console.Write($" -> {sum} ");
+        Console.WriteLine(" ");
+    }
+
+    static void Main(string[] args)
+    {
+        int[] array = new int[0];
+        while (true)
+        {
+            Console.Write("Введите значение массива: ");
+            string meaning = Console.ReadLine();
+            if (meaning != "end")
+            {
+                int number = Convert.ToInt32(meaning);
+                int[] newArray = array;
+                array = new int[array.Length + 1];
+                for (int i = 0; i < newArray.Length; i++)
+                {
+                    array[i] = newArray[i];
+                }
+                array[array.Length - 1] = number;
+            }
+            else
+            {
+                break;
+            }
+            WriteArray(array);
+        }
+
+    }
+}
